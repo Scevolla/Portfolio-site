35bc410cfc64366b0ee6df328590cff92e6c217c
Scevolla <moyaemailpochta@gmail.com>
2016-10-14 22:02:30 +0600

    Added sections resume and portfolio

--- _clean_html/js/common.js
+++ _clean_html/js/common.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 	
+	$("#portfolio_grid").mixItUp();
+
+	$(".portfolio_control_item").click(function() {
+		$(".portfolio_control_item").removeClass("active");
+		$(this).addClass("active");
+	});
+
 	function heightDetect() {
 		$('.main_head').css('height', $(window).height());
 	};
@@ -11,10 +18,15 @@ $(document).ready(function() {
 	});
 
 	$(".popup").magnificPopup({type:"image"});
+
 	$(".section_header").animated("fadeInUp", "fadeOutDown");
+
 	$(".section_about_center_col").animated("flipInY", "fadeOutDown");
 	$(".section_about_left_col").animated("fadeInLeft", "fadeOutDown");
 	$(".section_about_right_col").animated("fadeInRight", "fadeOutDown");
+
+	$(".resume_content_col_left .resume_content_item").animated("fadeInLeft", "fadeOutDown");
+	$(".resume_content_col_right .resume_content_item").animated("fadeInRight", "fadeOutDown");
 
 	$(".toggle_mnu").click(function() {
 		$(".sandwich").toggleClass("active");
